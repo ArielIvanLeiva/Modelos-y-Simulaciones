041063081726d38a02594464cc8c80c5fe44f59b
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2025-03-15 13:24:56 -0300

    Agregadas Soluciones ejercicios 7 y 8. También agregado script para recuperar ipynb

--- p2/main.py
+++ p2/main.py
@@ -23,4 +23,25 @@ for s in [2, 5, 10, 20]:
 
 
 # %%
+# Ejercicio 8
 
+# a)
+# Parámetro del processo de Poisson homogéneo: 3 * 1/minutos
+lamb = 3
+
+for s in [5, 10, 20, 30]:
+    # Como s está en segundos y lamb en minutos, convertimos s a minutos. mu es el parámetro de N(t) como v.a.
+    prob = poisson.cdf(1, mu=(lamb * s/60))
+    print(f"La probabilidad de que salga ileso si tarda {s} segundos es: {prob}")
+
+# %%
+
+# b)
+# Parámetro del processo de Poisson homogéneo: 3 * 1/minutos
+lamb = 3
+s1 = 5
+
+for s in [10, 20, 30]:
+    # Como s está en segundos y lamb en minutos, convertimos s a minutos. mu es el parámetro de N(t) como v.a.
+    prob = poisson.pmf(0, mu=(lamb * (s-s1)/60))
+    print(f"La probabilidad de que salga ileso si tarda {s} segundos es: {prob}")
