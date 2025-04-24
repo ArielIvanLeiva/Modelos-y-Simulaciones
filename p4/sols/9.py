3bd344aef33b7f17e1a45eb6565c1842b51d1303
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2025-04-23 22:15:52 -0300

    Agregada solución del ejercicio 9 del práctico 4.

--- p4/sols/9.py
+++ p4/sols/9.py
@@ -1,9 +1,22 @@
 # %%
 # Ejercicio 9
+import timeit
+from statistics import mean
 from numpy.random import random
+from numpy import log
+
+def get_rel_frecuencies(sample, keys=range(1, 10 + 1)):
+    frecuencies = {i: 0 for i in keys}
+
+    for x in sample:
+        if x in frecuencies.keys():
+            frecuencies[x] += 1
+
+    return {i: value / len(sample) for i, value in frecuencies.items()}
+
 
 # %%
-class Geometric():
+class Geom():
     def __init__(self, p):
         assert 0 <= p <= 1, f"p = {p}, wich is not a probability"
 
@@ -11,7 +24,10 @@ class Geometric():
 
     # a)
     def inv_method(self):
-        pass
+        p = self.p
+        u = random()
+        
+        return int(log(1-u)/log(1-p)) + 1
     
     # b)
     def full_sim_method(self):
@@ -23,8 +39,35 @@ class Geometric():
             tries += 1
             u = random()
             
-        return tries
+        return tries + 1
 
+# %%
+X = Geom(0.8)
+Y = Geom(0.2)
 
-X = Geometric(0.8)
-X.full_sim_method()
+num_of_executions = 10000
+
+def get_analysis(X: Geom, name):
+    print(f"Analysis for {name} (Geom({X.p})):")
+    
+    inv_time = timeit.timeit(X.inv_method, number=num_of_executions)/num_of_executions
+    full_sim_time = timeit.timeit(X.full_sim_method, number=num_of_executions)/num_of_executions
+
+    inv_sample = iter(X.inv_method() for _ in range(num_of_executions))
+    inv_mean = mean(inv_sample)
+
+    full_sim_sample = iter(X.full_sim_method() for _ in range(num_of_executions))
+    full_sim_mean = mean(full_sim_sample)
+
+    print(f"Average time/value from {num_of_executions} executions:")
+    print(f"Inverse transform method: \n\ttime: {inv_time},  value: {inv_mean}")
+    print(f"Full simulation: \n\ttime: {full_sim_time},  value: {full_sim_mean}")
+    print(f"Real expected value: E({name}) = {1/X.p}\n")
+
+get_analysis(X, "X")
+get_analysis(Y, "Y")
+
+# %%
+#get_rel_frecuencies([X.full_sim_method() for _ in range(100000)])
+#get_rel_frecuencies([X.inv_method() for _ in range(100000)])
+
