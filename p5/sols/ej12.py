268c87cf34a253c8a6b114a96b7f823e1d8e3b4d
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2025-05-03 19:10:38 -0300

    Agregada solución al ejercicio 12 del práctico 5.

---
+++ p5/sols/ej12.py
@@ -0,0 +1,42 @@
+# %%
+# Ejercicio 12
+from numpy.random import rand
+from numpy import log
+
+
+class Poisson:
+    def __init__(self, lam):
+        self.lam = lam
+
+    def sample(self, t):
+        arrivals = []
+        
+        time_past = 0
+        events = 0
+        while time_past <= t:
+            x = self.exp_sample()
+            time_past += x
+
+            if time_past > t:
+                break
+
+            events += 1
+            arrivals.append((events, time_past))
+
+        return (events, arrivals)
+
+    def exp_sample(self):
+        return -log(1 - rand()) / self.lam
+
+P = Poisson(3)
+P.sample(3)
+
+# %%
+# For testing purposes
+from statsUtils import probability_of
+
+t = 0.3
+n = 100000
+samp = [P.sample(t)[0] for _ in range(n)]
+
+probability_of(samp, lambda x: x > 1)
