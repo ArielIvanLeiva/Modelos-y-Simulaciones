dd363a5ad913b8470c5bf6680aed1f2d56bbd322
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2025-05-26 13:05:59 -0300

    Agregada a statutils una función auxiliar para imprimir gráficos de manera 'simple'.

--- statsutils/src/statutils/analysis.py
+++ statsutils/src/statutils/analysis.py
@@ -1,5 +1,6 @@
 from statistics import mean
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_rel_frecuencies(sample, keys=range(1, 10 + 1)):
     frecuencies = {i: 0 for i in keys}
@@ -29,3 +30,22 @@ def probability_of(sample, predicate):
 
 def get_ic_confidence(ics, real_value):
     return probability_of(ics, lambda x: x[0] <= real_value <= x[1])
+
+def plot_fun(fun, start, end, num=10000, limits=None, figsize=(6,4), ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=figsize, constrained_layout=True)
+
+    xs = np.linspace(start, end, num)
+    ys = [fun(x) for x in xs]
+    ax.plot(xs, ys)
+    
+    if limits:
+        ax.set_xlim(*limits[0])
+        ax.set_ylim(*limits[1])
+
+    ax.set_aspect('equal')
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+
+    return ax
